c47f95750e07ac332b985bac7244769131122653
Skyoohn <135787762+Skyoohn@users.noreply.github.com>
2024-02-29 20:46:58 +0900

    이게아닌가

--- AWS/build.gradle.kts
+++ AWS/build.gradle.kts
@@ -1,6 +1,29 @@
-plugins {
-    id("java")
+buildscript{
+    ext{
+        springBootVersion = '2.1.7.RELEASE'
+    }
+    repositories{
+        mavenCentral()
+    }
+    dependencies{
+        classpath("org.springframework.boot:spring-boot-" +
+                "gradle-plugin:${springBootVersion}")
+    }
 }
+plugins {
+    id('java')
+    id('eclipse')
+    id('org.springframework.boot')
+    id('io.spring.dependency-management')
+}
+repositories{
+    mavenCentral()
+}
+dependencies{
+    compile('org.springframework.boot:spring-boot-starter-web')
+    testCompile('org.springframework.boot:spring-boot-starter-test')
+}
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
